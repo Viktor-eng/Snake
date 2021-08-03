070bac63bc179bb49a74ca48b3f9813f345d3dc4
Виктор Белянкин <npockuma76@gmail.com>
2021-08-03 18:21:52 +0700

    Создали перегрузку конструктора, поняли пример инкапсуляции

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,32 +10,16 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
-
+            Point p1 = new Point(1, 3, '*');
             p1.DraW();
 
-            Point p2 = new Point();
-
-            
-
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
-
+            Point p2 = new Point(4,5, '#');
             p2.DraW();
 
             Console.ReadLine();
 
         }
 
-        static void DraW(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
+     
     }
 }
