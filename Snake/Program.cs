6755324b98754990291d09620763bc17111419c3
Виктор Белянкин <npockuma76@gmail.com>
2021-08-03 17:08:33 +0700

    Lesson-2/Function

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,6 +10,25 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            int x1 = 1;
+            int y1 = 3;
+
+            char sym1 = '*';
+            DraW(x1, y1, sym1);
+
+            int x2 = 4;
+            int y2 = 5;
+
+            char sym2 = '#';
+            DraW(x2, y2, sym2);
+
+
+        }
+
+        static void DraW(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
